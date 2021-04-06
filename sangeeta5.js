6fe222978465b9be6cc2908a08cf6b4383a47e81
Sangeetareddy30 <mothe.reddy@syf.com>
2021-04-06 12:40:49 +0000

    6th april

---
+++ sangeeta5.js
@@ -0,0 +1,15 @@
+//while loop excecutes zero or more times until condition fail
+//while checks condition and execute block
+
+let n = 1;
+while (n<10){
+    console.log("execute something");
+    n++;
+}
+//do while execute block and checks condition
+//do while executes one or more times until condition fail
+do{
+    console.log("execute something");
+    n++;
+}while (n<10)
+
